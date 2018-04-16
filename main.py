9e4fa99df766f20a245311ec0945e27e3a7fe2a9
huynq <qhuydtvt@gmail.com>
2018-04-16 16:28:14 +0700

    Added update class no

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
 import pyexcel as p
 from mongoengine import *
-import re
+from utils import remove_accent
 import mlab
 
 mlab.connect()
 
-def remove_accent(s):
-    s = re.sub(u'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
-    s = re.sub(u'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
-    s = re.sub(u'[èéẹẻẽêềếệểễ]', 'e', s)
-    s = re.sub(u'[ÈÉẸẺẼÊỀẾỆỂỄ]', 'E', s)
-    s = re.sub(u'[òóọỏõôồốộổỗơờớợởỡ]', 'o', s)
-    s = re.sub(u'[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]', 'O', s)
-    s = re.sub(u'[ìíịỉĩ]', 'i', s)
-    s = re.sub(u'[ÌÍỊỈĨ]', 'I', s)
-    s = re.sub(u'[ùúùụủũưừứựửữ]', 'u', s)
-    s = re.sub(u'[ƯỪỨỰỬỮÙÚỤỦŨ]', 'U', s)
-    s = re.sub(u'[ỳýỵỷỹ]', 'y', s)
-    s = re.sub(u'[ỲÝỴỶỸ]', 'Y', s)
-    s = re.sub(u'[Đ]', 'D', s)
-    s = re.sub(u'[đ]', 'd', s)
-    return s
 
-
-#
-#
 # placeholder_image = "http://via.placeholder.com/400x200"
 #
 
--- update-instructor-record.py
+++ update-instructor-record.py
@@ -6,17 +6,22 @@ import mlab
 
 mlab.connect()
 
+
 class InstructorRecord(Document):
     course = ObjectIdField()
+    className = StringField()
+    classNo = IntField()
     meta = {'collection': 'instructorrecords', 'strict': False}
+
 
 class Course(Document):
     name = StringField()
     description = StringField()
     meta = {'collection': 'courses', 'strict': False}
 
+
 def varify_course_id(course_name):
-    course_id = None;
+    course_id = None
     for course_obj in Course.objects():
         if course_obj.id == course_name:
             course_id = course_obj.id
@@ -24,21 +29,36 @@ def varify_course_id(course_name):
     return course_id
 
 
-# for index, record in enumerate(InstructorRecord.objects()):
-#     print(index + 1, end=' ')
-#     course_name = record.course
-#     course_id = varify_course_id(course_name)
-#     if course_id is not None:
-#         print(course_name, 'Checked')
-#         # print(course_name, 'has id: ', course_id)
-#         # print("Updating ", course_name, 'to: ', course_id)
-#         # record.update(set__course=course_id)
-#         # print('Done')
-#         # print("*"*20)
-#         # print("*"*20)
-#     else:
-#         print(course_name)
-        # print('*'*50, 'Unknow course: ', course_name)
+
+def convert_class_no():
+    phrase_to_remove = ["1.100", "F", "GRE", "1.", "2.", "3."]
+    with open("record_class_no.log", "w") as f:
+        for record in InstructorRecord.objects():
+            className = record.className
+            if className is None or className == "":
+                f.write(str(record.id) + "\n")
+            else:
+                splittedClassName = record.className.split(" ")
+                lastStringInClassName = splittedClassName[-1]
+                if len(splittedClassName) == 0:
+                    f.write(str(record.id) + "\n")
+                else:
+                    for p in phrase_to_remove:
+                        lastStringInClassName = lastStringInClassName.replace(p, "")
+                    if lastStringInClassName == "":
+                        f.write(str(record.id) + "\n")
+                    else:
+                        print("Trying to convert ...")
+                        class_no = int(lastStringInClassName)
+                        print(class_no)
+                        record.update(set__classNo=class_no)
+                        record.reload()
+                        if record.classNo == class_no:
+                            print("Updated successfully")
+                        else:
+                            print("Did not work")
+                        #f.write(lastStringInClassName + "\n")
 
 
-# TODO: update Intensive course, findout an "underfined" course
+if __name__ == "__main__":
+    convert_class_no()
